923fbfdaf38f6877fd26e705ba0616a619f6286e
navedkhan012 <navedkhan012@gmail.com>
2023-08-27 17:05:17 +0530

    37 work on check out page

--- frontend/src/screens/Checkout.js
+++ frontend/src/screens/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
 import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +10,8 @@ import { getAddress } from "../store/actions/useraddress";
  **/
 
 const Checkout = (props) => {
+  const [selectAddress, setSelectAddress] = useState(null);
+
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
   const useAddress = useSelector((state) => state.useAddress);
@@ -17,7 +19,7 @@ const Checkout = (props) => {
   useEffect(() => {
     dispatch(getAddress());
   }, [dispatch]);
-
+  console.log("selectAddress", selectAddress);
   return (
     <Layout>
       <Container>
@@ -31,7 +33,26 @@ const Checkout = (props) => {
             <div>
               <h3>Address </h3>
               {useAddress.payload.address.map((address, i) => (
-                <h6 key={i}>{address.address}</h6>
+                <div>
+                  <div>
+                    <input
+                      type="radio"
+                      id={i}
+                      name="address"
+                      value={address.address}
+                      onClick={() => setSelectAddress(address)}
+                    />
+                      <label for={i}>{address.address}</label>
+                  </div>
+                  {/* <input
+                    type="radio"
+                    name="address"
+                    value={address.address}
+                    onClick={() => setSelectAddress(address)}
+                  /> */}
+                  {/* <h6 key={i}>{address.address}</h6> */}
+                  <button className="btn btn-primary">Devlivery here</button>
+                </div>
               ))}
             </div>
             <hr />
